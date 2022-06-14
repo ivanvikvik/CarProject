19ada6353f40a8290818330d9cd1849d61065ed1
Victor <ivanvikvik@gmail.com>
2022-06-14 20:40:04 +0300

    Update ConsolePrinter class

--- src/by/itstep/vikvik/javalessons/lesson15/view/ConsolePrinter.java
+++ src/by/itstep/vikvik/javalessons/lesson15/view/ConsolePrinter.java
@@ -2,11 +2,6 @@ package by.itstep.vikvik.javalessons.lesson15.view;
 
 public class ConsolePrinter {
     public void print(String msg) {
-    // file
-        // open
-        //write
-        //close
-
         System.out.print(msg);
     }
 }
